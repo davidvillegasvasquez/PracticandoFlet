b9ca89545ad420e724f965c8c729728c1d5463de
davidvillegasvásquez <davidvillegas1@hotmail.com>
2026-04-18 23:16:43 -0400

    260418: se comenzó la práctica con el control dropdown para autor y sus libros.

---
+++ src/entradas/apiAutores.py
@@ -0,0 +1,68 @@
+#Consumidor de api rest django, apiAutores.py
+import flet as ft
+import requests
+
+def principal(pagina: ft.Page):
+    pagina.title = "Consumo API Django"
+
+    def botonClickeado(e):
+        # 1. Consumir la API.  Algunos de los atributos y métodos más utilizados del objeto `request` incluyen:
+        try:
+            respuestaGet = requests.get("http://127.0.0.1:8000/catalogo/api-todosLosAutores/") #respuestaGet es un objeto Response.
+            
+        except:
+            txt_resultado.value = "hubo un error en conexión"
+        else:
+        # Algunos de los atributos y métodos más utilizados del objeto Response del modulo requests incluyen:
+            #headers
+            print("respuestaGet.text:")
+            print(respuestaGet.text)
+            print("    ***")
+
+            print("respuestaGet.status_code:")
+            print(respuestaGet.status_code)
+            print("    ***")
+
+            print("respuestaGet.url:")
+            print(respuestaGet.url)
+            print("    ***")
+
+            print("respuestaGet.headers:")
+            print(respuestaGet.headers)
+            print("    ***")
+
+            resultadoGet=""
+
+            diccionario = respuestaGet.json() #Analiza el cuerpo de la respuesta como JSON y devuelve un diccionario o lista de Python.
+
+            #Tomamos la cant de autores primero, puesto que vamos a concatenar:
+            resultadoGet = f"cantidad de autores:{diccionario['count']}, "
+
+            #Tomamos el primer elemento de results que es una lista de diccionarios:
+            listDeDicts = diccionario['results']  
+
+            #Filtramos listDeDicts para extraer los campos deseados de cada uno de los diccionarios que contienen los datos del autor:
+            listaFiltrada = [{"id": d["id"], "nombre": d["nombre"], "apellido": d["apellido"], "nacimiento": d["nacimiento"], "muerte": d["muerte"]} for d in listDeDicts]
+
+            #Extraemos los pares clave-valor de la lista de diccionarios y los concatenamos en resultadoGet:
+            for diccionario in listaFiltrada:
+                for clave, valor in diccionario.items():
+                    resultadoGet += f"{clave}: {valor}, "
+
+            # 4. Por último depositamos el string en el atributo value del control:
+            txt_resultado.value = resultadoGet
+        
+        finally:
+            # 5. Claro, finalmente actualizamos la pag para ver los resultados:
+            pagina.update()
+
+    txt_resultado = ft.Text()
+    btn_cargar = ft.Button("Cargar Datos", on_click=botonClickeado)
+
+    def borrar_texto(e):
+        txt_resultado.value = ""  # O texto.value = None
+        pagina.update()
+
+    pagina.add(btn_cargar, txt_resultado, ft.Button("Borrar", on_click=borrar_texto))
+
+ft.run(principal)
--- src/main.py
+++ src/main.py
@@ -1,51 +1,80 @@
-#Consumidor de api rest django, fletDrfClient.py
+#Menú desplegable (dropdown) de autores y sus libros.
 import flet as ft
 import requests
 
 def principal(pagina: ft.Page):
-    pagina.title = "Consumo API Django"
+    pagina.title = "Menú desplegable autor y sus libros"
 
     def botonClickeado(e):
-        # 1. Consumir la API
+        # 1. Consumir la API.  Algunos de los atributos y métodos más utilizados del objeto `request` incluyen:
         try:
-            respuesta = requests.get("http://127.0.0.1:8000/catalogo/api-todosLosAutores/")
+            respuestaGet = requests.get("http://127.0.0.1:8000/catalogo/api-todosLosAutores/") #respuestaGet es un objeto Response.
+            
         except:
-            txt_resultado.value = "hubo un error en conexión"
+            pagina.show_dialog(ft.AlertDialog(
+                title=ft.Text("Hubo un error en la conexión."),
+                actions=[ft.TextButton("Cerrar", on_click=lambda e: pagina.pop_dialog())],
+                modal=True
+            ))
         else:
-            resultado=""
-            data = respuesta.json()
 
-            # 2. Convertimos el json a dict
-            diccionario = dict(data)
-
-            #Tomamos la cant de autores primero, puesto que vamos a concatenar:
-            resultado = f"cantidad de autores:{diccionario['count']}, "
-
+            diccionario = respuestaGet.json() #Analiza el cuerpo de la respuesta como JSON y devuelve un diccionario o lista de Python.
+            
             #Tomamos el primer elemento de results que es una lista de diccionarios:
-            listDeDicts = diccionario['results']  
+            listDeDicts = diccionario['results'] 
+   
+            #Filtramos listDeDicts para extraer los campos deseados de cada uno de los diccionarios que contienen los datos del autor:
+            listaFiltrada = [{"id": d["id"], "nombre": d["nombre"], "apellido": d["apellido"], "libros": d["libros"]} for d in listDeDicts]
 
-            #Filtramos listDeDict para extraer los campos deseados de cada uno de los diccionarios que contienen los datos del autor:
-            listaFiltrada = [{"id": d["id"], "nombre": d["nombre"], "apellido": d["apellido"], "nacimiento": d["nacimiento"], "muerte": d["muerte"]} for d in listDeDicts]
+            #Así convertimos una lista de diccionarios a una lista de ft.dropdown.Option:
+            dropdown_options_autores = [
+                ft.dropdown.Option(
+                    key=autor["id"],      # El valor que se obtiene al seleccionar
+                    text=f"{autor['nombre']} {autor['apellido']}" #Así hacemos un atributo text compuesto. 
+                )
+            for autor in listaFiltrada
+            ]
+            menuAutores.options = dropdown_options_autores
 
-            #Extraemos los pares clave-valor de la lista de diccionarios y los concatenamos en resultado:
-            for diccionario in listaFiltrada:
-                for clave, valor in diccionario.items():
-                    resultado += f"{clave}: {valor}, "
+            #Y para sus libros:
+            dropdown_options_libros = [
+                ft.dropdown.Option(
+                    key=libro["id"],      # El valor que se obtiene al seleccionar
+                    text=libro["libros"]
+                )
+            for libro in listaFiltrada
+            ]
+            menuLibros.options = dropdown_options_libros
 
-            # 4. Por último depositamos el string en el atributo value del control:
-            txt_resultado.value = resultado
-        
         finally:
-            # 5. Claro, finalmente actualizamos la pag para ver los resultados:
-            pagina.update()
+            #Claro, finalmente actualizamos la pag para ver los resultados:
+            pagina.update()          
 
-    txt_resultado = ft.Text()
+    menuAutores = ft.Dropdown(
+                        editable=True,                            
+                        width=220,
+                        label="Autores",
+                        options=[],
+                        #on_select=handle_dropdown_select_autor,
+                    )
+
+    menuLibros = ft.Dropdown(
+                        editable=False,                            
+                        width=220,
+                        label="Libros",
+                        options=[],
+                    )
+    """
+    def handle_dropdown_select_autor(e: ft.Event[ft.Dropdown]):
+        e.control.autor = e.control.value
+    """
     btn_cargar = ft.Button("Cargar Datos", on_click=botonClickeado)
 
-    def borrar_texto(e):
-        txt_resultado.value = ""  # O texto.value = None
-        pagina.update()
+    def botonBorrarClickeado(e):
+        menuAutores.options=[]
+        menuLibros.options=[]
+        pagina.update() 
 
-    pagina.add(btn_cargar, txt_resultado, ft.Button("Borrar", on_click=borrar_texto))
+    pagina.add(btn_cargar, ft.Row(controls=[menuAutores, menuLibros]), ft.Button("Borrar", on_click=botonBorrarClickeado))
 
 ft.run(principal)
